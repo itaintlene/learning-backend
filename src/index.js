8baf09719dadf3ec4563e8478b2fc95139ceb77f
itaintlene <105277093+itaintlene@users.noreply.github.com>
2024-11-13 20:35:58 +0000

    util config

--- src/index.js
+++ src/index.js
@@ -6,8 +6,22 @@ dotenv.config({
 })
 
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 connectDB()
+.then(()=>{
+    app.on("error", (error)=> {
+        console.log("ERR:", error)
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at PORT ${process.env.PORT}`)
+    })
+})
+.catch((err)=> {
+    console.log("MongoDB connection Failed!!", err)
+})
 
 /* 
 import express from "express"
